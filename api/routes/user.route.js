de9eb85c965a4281b931c51dabd8c1e29ef9dbd0
ualiu <urim.aliu@gmail.com>
2023-02-28 13:53:46 -0500

    created routes and controllers

---
+++ api/routes/user.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+// import { deleteUser } from "../controllers/user.controller.js";
+// import { verifyToken } from "../middleware/jwt.js";
+
+const router = express.Router();
+
+router.get("/test")
+
+// router.delete("/:id", verifyToken, deleteUser);
+// router.get("/:id", getUser);
+
+export default router;
