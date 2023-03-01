67eb467b3c814af2513e7d7795ea1935e492ffed
ualiu <urim.aliu@gmail.com>
2023-02-28 21:22:03 -0500

    jwt token authentication isntalled

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req,res) => {
  
@@ -28,8 +29,15 @@ export const login = async (req,res) => {
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect) return res.status(404).send("Wrong password or username!")
 
+    const token = jwt.sign({
+      id: user._id, 
+      isSeller: user.isSeller,
+    }, process.env.JWT_KEY)
+
     const {password, ...info} = user._doc
-    res.status(200).send(user)
+    res.cookie("accessToken", token, {
+      httpOnly: true,
+    }).status(200).send(user)
 
   } catch (err) {
     res.status(500).send("something went wrong!")
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,13 @@
-export const fn = (req,res) => {
- 
-}
+import User from "../models/user.model.js"
+
+
+export const deleteUser = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (req.userId !== user._id.toString()) {
+    return res.status(403).send("You can delete only your account!");
+  }
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("deleted.");
+};
 
